7611f3ffc908c4324d4ea0a10722b36ba04c752a
Kachiko_2 <Kachiko_2@Kachiko-Serveur.home>
2015-10-31 12:18:23 +0100

    initialisation projet

---
+++ comptabilite/src/main/java/org/personnel/kachiko/comptabilite/App.java
@@ -0,0 +1,13 @@
+package org.personnel.kachiko.comptabilite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
